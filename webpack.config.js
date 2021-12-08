8569e92fd89e3a58f4ee011f8d53666f797ad5e9
Gary Bortosky <garybort@gmail.com>
2021-12-08 15:13:31 -0600

    steal webconfig from sensor-play

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,108 @@
-'use strict';
-let path = require('path');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
-module.exports = {
-  entry: require('./webpack/entry'),
+const { merge } = require('webpack-merge');
 
-  context: path.join(process.cwd(), 'src'),
-
-  output: require('./webpack/output'),
-
-  module: require('./webpack/module'),
-
-  plugins: require('./webpack/plugins'),
-
-  resolve: require('./webpack/resolve'),
-
-  // devServer: require('./webpack/dev-server'),
-
-  stats: 'verbose',
-
-  devtool: 'source-map'
+const base = {
+  entry: {
+    'sensor-play': ['scripts/app.ts'],
+  },
+  context: path.resolve(__dirname, 'src'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'scripts/[name].[chunkhash:8].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: { onlyCompileBundledFiles: true },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+      scriptLoading: 'defer',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+      options: {
+        concurrency: 100,
+      },
+    }),
+    new InjectManifest({
+      swSrc: 'sw.js',
+      swDest: 'service-worker.js',
+      exclude: ['web.config'],
+    }),
+  ],
+  resolve: {
+    modules: ['node_modules', path.resolve(__dirname, 'src')],
+    extensions: ['.ts', '.js', 'scss'],
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 };
+
+const prod = {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  output: {
+    publicPath: '/',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[chunkhash:8].css',
+      chunkFilename: 'css/[id].[chunkhash:8].css',
+    }),
+  ],
+};
+
+const dev = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  output: {
+    publicPath: 'http://localhost:8080/',
+  },
+  stats: {
+    colors: true,
+  },
+  devServer: {
+    client: {
+      logging: 'info',
+    },
+    port: 8080,
+  },
+  devtool: 'inline-source-map',
+};
+
+module.exports = (env) => merge(base, env && env.production ? prod : dev);
