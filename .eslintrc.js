53c07317701e79782867413c56969a782627957c
Gary Bortosky <garybort@gmail.com>
2021-12-08 16:52:44 -0600

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,32 @@
-'use strict';
-
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
-  plugins: [
-    '@typescript-eslint',
-    'prettier',
-  ],
-  rules: {
-    'prettier/prettier': 1,
-    'sort-keys': 1,
-    '@typescript-eslint/indent': 0,
-    '@typescript-eslint/member-ordering': [
-      2,
-      {
-        default: [
-          'public-static-field',
-          'protected-static-field',
-          'private-static-field',
-          'public-instance-field',
-          'protected-instance-field',
-          'private-instance-field',
-          'public-constructor',
-          'protected-constructor',
-          'private-constructor',
-          'public-instance-method',
-          'protected-instance-method',
-          'private-instance-method',
-          'public-static-method',
-          'protected-static-method',
-          'private-static-method',
-        ],
-      },
-    ],
-    '@typescript-eslint/no-parameter-properties': 0,
-    '@typescript-eslint/no-unused-vars': 0,
+    project: ['tsconfig.json'],
   },
   env: {
     browser: true,
-    node: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  overrides: [
+    {
+      files: ['**/*.spec.ts'],
+      rules: {
+        '@typescript-eslint/unbound-method': 'off',
+        'jest/unbound-method': 'error',
+      },
+    },
+  ],
+  rules: {
+    '@typescript-eslint/explicit-member-accessibility': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/member-ordering': 'warn',
+    '@typescript-eslint/unbound-method': 'error',
   },
 };
