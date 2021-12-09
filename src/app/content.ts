d989f68d9859cae7f48e50e71ed51e554b64d8e5
Gary Bortosky <garybort@gmail.com>
2021-12-09 08:48:07 -0600

    move src to new structure

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,1 +1,51 @@
-console.log('content');
+import { Message } from './types';
+
+// document.body.innerHTML = "";
+
+function addButton(name: string, cb: () => void) {
+  const a = document.createElement('button');
+  a.innerText = name;
+  a.onclick = cb;
+  document.body.appendChild(document.createElement('br'));
+  document.body.appendChild(a);
+}
+
+function log(str: string) {
+  console.log(str);
+  logDiv.innerHTML += str + '<br>';
+}
+
+addButton('Clear logs', () => {
+  logDiv.innerHTML = '';
+});
+
+addButton('Send message with delayed response', () => {
+  const message: Message = { delayedResponse: true };
+  chrome.runtime.sendMessage(message, (response) => {
+    log('Background page responded: ' + response);
+  });
+});
+
+addButton('Show counters', () => {
+  const message: Message = { getCounters: true };
+  chrome.runtime.sendMessage(message, (response) => {
+    log('In-memory counter is: ' + response.counter);
+    log('Persisted counter is: ' + response.persistentCounter);
+  });
+});
+
+addButton('Set an alarm', () => {
+  const message: Message = { setAlarm: true };
+  chrome.runtime.sendMessage(message);
+});
+
+chrome.runtime.onMessage.addListener((msg, _, _sendResponse) => {
+  log('Got message from background page: ' + msg);
+});
+
+const logDiv = document.createElement('div');
+logDiv.style.border = '1px dashed black';
+document.body.appendChild(document.createElement('br'));
+document.body.appendChild(logDiv);
+
+log('Ready.');
--- src/app/types.ts
+++ src/app/types.ts
@@ -1,4 +1,3 @@
-
 export interface Message {
   setAlarm?: boolean;
   delayedResponse?: boolean;
